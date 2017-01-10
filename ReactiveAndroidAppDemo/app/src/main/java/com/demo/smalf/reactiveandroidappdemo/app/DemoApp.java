2ad9debaf5c921bc8e16844adbd7a2bee6e3bdb4
smalofeev <smal@Pearl.softservecom.com>
2017-01-10 17:12:25 +0200

    added application and gadle dependancies

---
+++ ReactiveAndroidAppDemo/app/src/main/java/com/demo/smalf/reactiveandroidappdemo/app/DemoApp.java
@@ -0,0 +1,9 @@
+package com.demo.smalf.reactiveandroidappdemo.app;
+
+/**
+ * Demo application implementation, responsible for management of global shared components.
+ *
+ * @author Serhiy Malofeev
+ */
+public class DemoApp {
+}
